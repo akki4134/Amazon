776de369205645bc7b26f93a4740e9e65f088842
akki4134 <yellagandulaakhil@gmail.com>
2021-05-28 18:33:36 +0530

    first

---
+++ portal/src/screens/Home.js
@@ -0,0 +1,28 @@
+import './Home.css'
+import Slider from '../components/Slider'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Slider />
+            <div className="home" >
+
+
+                <div>
+
+                  
+
+
+
+
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Home
